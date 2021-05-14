23097ba1c2133531a96cf295dadfe64a17c1846a
beep-create <77546458+beep-create@users.noreply.github.com>
2021-05-13 21:15:38 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function loadFlag() 
+{ document.getElementById("imgFlagName").src= "flagimgs/"+document.getElementById("inputCountryName").value + ".png";
+ console.log(document.getElementById("inputCountryName").value); 
+ console.log(document.getElementById("imgFlagName").src); 
+}
